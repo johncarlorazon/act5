babb0b751305a9b9f3f06612b4f32315f5979cae
johncarlorazon <143053532+johncarlorazon@users.noreply.github.com>
2023-11-18 15:35:32 +0800

    initial change

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, ScrollView, Button, Image } from 'react-native';
+import { styles } from './styles/styles';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const userDataWithImages = [
+  {
+    firstName: 'Arnel',
+    lastName: 'Casanoba',
+    nickname: 'Aniw',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    image: require('./assets/aniw.jpg'), 
   },
-});
+  {
+    firstName: 'Cegie',
+    lastName: 'Simacon',
+    nickname: 'siji',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    image: require('./assets/ciji.jpg'), 
+  },
+  {
+    firstName: 'Christian Joven',
+    lastName: 'Racho',
+    nickname: 'jobin',
+    course: 'BSELECT',
+    year: 'Third Year',
+    image: require('./assets/joven.jpg'),
+  },
+  {
+    firstName: 'Kim Micheal',
+    lastName: 'Ranario',
+    nickname: 'kem',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    image: require('./assets/kim.jpg'), 
+  },
+  {
+    firstName: 'Christopher',
+    lastName: 'Magwan',
+    nickname: 'opi',
+    course: 'BSCRIM',
+    year: 'Third Year',
+    image: require('./assets/opi.jpg'),
+  },
+  {
+    firstName: 'John Carlo',
+    lastName: 'Razon',
+    nickname: 'carlo',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/me.jpg'), 
+  },
+];
+
+const UserList = () => {
+  const [selectedUser, setSelectedUser] = React.useState(null);
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => {
+      setSelectedUser(item);
+      setModalVisible(true);
+    }}>
+      <Text style={styles.nickname}>{item.nickname}</Text>
+    </TouchableOpacity>
+  );
+
+  <View style={styles.container}>
+       <Text style={styles.pt}>Team Hamal</Text>
+      <FlatList
+        data={userDataWithImages}
+        keyExtractor={(item) => item.nickname}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          {selectedUser && (
+            <ScrollView>
+              <Text style={styles.fullName}>
+                {selectedUser.firstName} {selectedUser.lastName}
+              </Text>
+              <Text>Nickname: {selectedUser.nickname}</Text>
+              <Text>Course: {selectedUser.course}</Text>
+              <Text>Year: {selectedUser.year}</Text>
+              {selectedUser.image && <Image source={selectedUser.image} style={{ width: 200, height: 200 }} />}
+              <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
+            </ScrollView>
+          )}
+        </View>
+      </Modal>
+    </View>
+  
+};
+
+export default UserList;   
---
+++ styles/styles.jsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  nickname: {
+    fontSize: 20,
+    margin: 10,
+    color: 'blue',
+    textAlign: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fullName: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+    color: 'blue',
+  },
+  pt:{
+    fontSize:40,
+    fontWeight:'bolder',
+
+  }
+
+});
